7155ce13d94686053ae310cfd7f3e07fc8a97c2b
Jared S <jaredjames2020@gmail.com>
2020-03-31 16:46:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-// // Your code here
-// let dodger = document.getElementById("dodger");
-//   dodger.style.backgroundColor = "#000000";
-//   dodger.style.backgroundColor = "#FF69B4";
-//   dodger.style.left;
-//   dodger.style.bottom;
-//   dodger.style.bottom = "100px";
-//   dodger.style.bottom = "0px";
+// Your code here
+let dodger = document.getElementById("dodger");
+  dodger.style.backgroundColor = "#000000";
+  dodger.style.backgroundColor = "#FF69B4";
+  dodger.style.left;
+  dodger.style.bottom;
+  dodger.style.bottom = "100px";
+  dodger.style.bottom = "0px";
   
 // document.addEventListener("keydown", function(e) {
 //   console.log(e.key);
