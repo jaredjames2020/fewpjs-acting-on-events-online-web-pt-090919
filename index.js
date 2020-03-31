66e09667ec67457200c34cb9ec30e4580fd63971
Jared S <jaredjames2020@gmail.com>
2020-03-31 16:48:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,6 +29,15 @@ function moveDodgerLeft() {
   }
 }
 
+function moveDodgerright() {
+  let rightNumbers = dodger.style.right.replace("px", "");
+  let right = parseInt(rightNumbers, 10);
+ 
+  if (right > 0) {
+    dodger.style.right = `${right - 10}px`;
+  }
+}
+
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
     moveDodgerLeft();
