3114640e1ba226c6bdad56fbd3d0181eef84adec
Jared S <jaredjames2020@gmail.com>
2020-03-31 16:46:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,4 +20,11 @@ document.addEventListener("keydown", function(e) {
   }
 });
 
-let dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
