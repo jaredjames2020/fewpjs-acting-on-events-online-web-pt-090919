ec67bdac06af841591d25bc4cbad693e62ce41eb
Jared S <jaredjames2020@gmail.com>
2020-03-31 16:51:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function moveDodgerRight() {
   let left = parseInt(leftNumbers, 10);
  
   if (left > 0) {
-    dodger.style.right = `${left + 10}px`;
+    dodger.style.left = `${left + 10}px`;
   }
 }
 
